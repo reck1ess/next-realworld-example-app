65a21779f3a6fc9822732efc9ed0e1b050e896e6
reck1ess <heeinso@snu.ac.kr>
2020-01-26 22:24:50 +0900

    feat: Implement a CommentList component

---
+++ components/comment/CommentList.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+
+import RedError from "../../components/common/RedError";
+import fetcher from "../../lib/utils/fetcher";
+import { SERVER_BASE_URL } from "../../lib/utils/constant";
+import LoadingSpinner from "../../components/common/LoadingSpinner";
+import Comment from "./Comment";
+import CommentInput from "./CommentInput";
+
+const CommentList = () => {
+  const router = useRouter();
+  const {
+    asPath,
+    query: { pid }
+  } = router;
+
+  const { data: commentData, error: commentError } = useSWR(
+    `${SERVER_BASE_URL}/articles/${pid}/comments`,
+    fetcher
+  );
+
+  if (!commentData) {
+    return <LoadingSpinner />;
+  }
+
+  if (commentError)
+    return (
+      <RedError message="Cannot load comments related to this article..." />
+    );
+
+  const { comments } = commentData;
+
+  return (
+    <div>
+      <CommentInput />
+      {comments.map(comment => (
+        <Comment key={comment.id} comment={comment} />
+      ))}
+    </div>
+  );
+};
+
+export default CommentList;
