75e17e402ed31d238f558394e71a60354ca326b6
reck1ess <heeinso@snu.ac.kr>
2020-01-26 22:24:24 +0900

    feat: Implement a DeleteButton component

---
+++ components/comment/DeleteButton.js
@@ -0,0 +1,38 @@
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import marked from "marked";
+import fetch from "isomorphic-unfetch";
+
+import ArticleMeta from "../../components/article/ArticleMeta";
+import RedError from "../../components/common/RedError";
+import fetcher from "../../lib/utils/fetcher";
+import { SERVER_BASE_URL } from "../../lib/utils/constant";
+import LoadingSpinner from "../../components/common/LoadingSpinner";
+
+const DeleteButton = ({ commentId }) => {
+  const router = useRouter();
+  const {
+    query: { pid }
+  } = router;
+
+  const handleDelete = async commentId => {
+    const { error } = useSWR(
+      `${SERVER_BASE_URL}/articles/${pid}/comments/${commentId}`,
+      url =>
+        fetch(url, {
+          method: "DELETE"
+        })
+    );
+    if (error) {
+      Toast.info(`Fail to delete a comment...`);
+    }
+  };
+
+  return (
+    <span className="mod-options">
+      <i className="ion-trash-a" onClick={() => handleDelete(commentId)} />
+    </span>
+  );
+};
+
+export default DeleteButton;
