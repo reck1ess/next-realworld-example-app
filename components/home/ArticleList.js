a13f9b7fb88b3147b6d0e6dea3bb193ded7ec1a8
reck1ess <heeinso@snu.ac.kr>
2020-01-26 22:22:55 +0900

    feat: Implement an ArticleList component

---
+++ components/home/ArticleList.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import ArticlePreview from "./ArticlePreview";
+import Pagination from "./Pagination";
+import LoadingSpinner from "../common/LoadingSpinner";
+
+const ArticleList = ({
+  loading,
+  articles,
+  totalPagesCount,
+  currentPage,
+  onSetPage
+}) => {
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
+  if (articles && articles.length === 0) {
+    return <div className="article-preview">No articles are here... yet.</div>;
+  }
+
+  return (
+    <div>
+      {articles &&
+        articles.map(article => (
+          <ArticlePreview key={article.slug} article={article} />
+        ))}
+
+      <Pagination
+        totalPagesCount={totalPagesCount}
+        currentPage={currentPage}
+        onSetPage={onSetPage}
+      />
+    </div>
+  );
+};
+
+export default ArticleList;
