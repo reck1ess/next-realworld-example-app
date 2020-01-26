c577bdeb619017584f83269a7d4096bb38ddc48b
reck1ess <heeinso@snu.ac.kr>
2020-01-26 22:09:33 +0900

    feat: Implement a RedError component

---
+++ components/common/RedError.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const RedError = ({ message }) => (
+  <React.Fragment>
+    <div className="error-container">
+      <div className="error-content">{message}</div>
+    </div>
+    <style jsx>
+      {`
+        .error-container {
+          display: flex;
+          justify-content: center;
+        }
+        .error-content {
+          display: inline-block;
+          margin: 20px auto;
+          border-radius: 4px;
+          padding: 8px 15px;
+          color: #f02d2d;
+          font-weight: 600
+          background-color: rgba(240, 45, 45, 0.1);
+        }
+      `}
+    </style>
+  </React.Fragment>
+);
+
+export default RedError;
