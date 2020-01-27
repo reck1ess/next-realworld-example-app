18651877144b50900889a64f4029431920be1095
reck1ess <heeinso@snu.ac.kr>
2020-01-27 22:59:54 +0900

    feat: Implement a login page

---
+++ pages/login.js
@@ -0,0 +1,84 @@
+import Router from "next/router";
+import React from "react";
+
+import ListErrors from "../components/common/ListErrors";
+import CustomLink from "../components/common/CustomLink";
+import api from "../lib/api";
+
+const Login = () => {
+  const [isLoading, setLoading] = React.useState(false);
+  const [errors, setErrors] = React.useState([]);
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleEmailChange = e => setEmail(e.target.value);
+  const handlePasswordChange = e => setPassword(e.target.value);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    setLoading(true);
+
+    const { ok, data } = await api.Auth.login(email, password);
+
+    setLoading(false);
+    setErrors(!ok ? data.errors : []);
+
+    if (data && data.user)
+      window.localStorage.setItem(`user`, data && JSON.stringify(data.user));
+
+    if (ok) {
+      Router.push(`/`);
+    }
+  };
+
+  return (
+    <div className="auth-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Sign in</h1>
+            <p className="text-xs-center">
+              <CustomLink href="/register">Need an account?</CustomLink>
+            </p>
+
+            <ListErrors errors={errors} />
+
+            <form onSubmit={handleSubmit}>
+              <fieldset>
+                <fieldset className="form-group">
+                  <input
+                    className="form-control form-control-lg"
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={handleEmailChange}
+                  />
+                </fieldset>
+
+                <fieldset className="form-group">
+                  <input
+                    className="form-control form-control-lg"
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                  />
+                </fieldset>
+
+                <button
+                  className="btn btn-lg btn-primary pull-xs-right"
+                  type="submit"
+                  disabled={isLoading}
+                >
+                  Sign in
+                </button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
