1129ec187142dffca5282856f96e863cd0ff4cbe
reck1ess <heeinso@snu.ac.kr>
2020-02-01 20:23:41 +0900

    feat: Implement a FollowUserButton component

---
+++ components/profile/FollowUserButton.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const FollowUserButton = ({
+  isUser,
+  following,
+  username,
+  follow,
+  unfollow
+}) => {
+  if (isUser) {
+    return null;
+  }
+
+  const handleClick = e => {
+    e.preventDefault();
+    following ? unfollow(username) : follow(username);
+  };
+
+  return (
+    <button
+      className={`btn btn-sm action-btn ${
+        following ? "btn-secondary" : "btn-outline-secondary"
+      }`}
+      onClick={handleClick}
+    >
+      <i className="ion-plus-round" />
+      &nbsp;
+      {following ? "Unfollow" : "Follow"} {username}
+    </button>
+  );
+};
+
+export default FollowUserButton;
