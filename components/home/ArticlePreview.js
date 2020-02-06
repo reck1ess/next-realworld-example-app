09735be7ed946ed8c11fcaeefeb04db22f3255d3
reck1ess <heeinso@snu.ac.kr>
2020-02-06 23:28:47 +0900

    refactor: Modify an ArticlePreview component

--- components/home/ArticlePreview.js
+++ components/home/ArticlePreview.js
@@ -1,61 +1,102 @@
-import React from "react";
 import Link from "next/link";
+import React from "react";
+import useSWR, { mutate, trigger } from "swr";
+import fetch from "isomorphic-unfetch";
 
-import api from "../../lib/api";
 import CustomLink from "../common/CustomLink";
+import checkLogin from "../../lib/utils/checkLogin";
+import storage from "../../lib/utils/storage";
+import fetcher from "../../lib/utils/fetcher";
+import { SERVER_BASE_URL } from "../../lib/utils/constant";
 
 const FAVORITED_CLASS = "btn btn-sm btn-primary";
 const NOT_FAVORITED_CLASS = "btn btn-sm btn-outline-primary";
 
 const ArticlePreview = ({ article }) => {
-  const handleClickFavorite = e => {
-    e.preventDefault();
-    if (article.favorited) {
-      api.Articles.unfavorite(article.slug);
-    } else {
-      api.Articles.favorite(article.slug);
+  const [preview, setPreview] = React.useState(article);
+  const { data: currentUser } = useSWR("user", storage);
+  const isLoggedIn = checkLogin(currentUser);
+
+  const handleClickFavorite = async slug => {
+    if (!isLoggedIn) return;
+
+    setPreview({
+      ...preview,
+      favorited: !preview.favorited,
+      favoritesCount: preview.favorited
+        ? preview.favoritesCount - 1
+        : preview.favoritesCount + 1
+    });
+
+    try {
+      const response = await fetch(
+        `${SERVER_BASE_URL}/articles/${slug}/favorite`,
+        {
+          method: `${preview.favorited ? "DELETE" : "POST"}`,
+          headers: {
+            Authorization: `Token ${currentUser.token}`
+          }
+        }
+      );
+    } catch (error) {
+      setPreview({
+        ...preview,
+        favorited: !preview.favorited,
+        favoritesCount: preview.favorited
+          ? preview.favoritesCount - 1
+          : preview.favoritesCount + 1
+      });
     }
   };
-
-  const favoriteButtonClass = article.favorited
-    ? FAVORITED_CLASS
-    : NOT_FAVORITED_CLASS;
 
   if (!article) return;
 
   return (
     <div className="article-preview">
       <div className="article-meta">
-        <CustomLink href={`/@${article.author.username}`}>
-          <img src={article.author.image} alt="" />
+        <CustomLink href={`/profile/${preview.author.username}`}>
+          <img src={preview.author.image} alt="" />
         </CustomLink>
 
         <div className="info">
-          <CustomLink className="author" href={`/@${article.author.username}`}>
-            {article.author.username}
+          <CustomLink
+            className="author"
+            href={`/profile/${preview.author.username}`}
+          >
+            {preview.author.username}
           </CustomLink>
           <span className="date">
-            {new Date(article.createdAt).toDateString()}
+            {new Date(preview.createdAt).toDateString()}
           </span>
         </div>
 
         <div className="pull-xs-right">
-          <button className={favoriteButtonClass} onClick={handleClickFavorite}>
-            <i className="ion-heart" /> {article.favoritesCount}
+          <button
+            className={
+              preview.favorited ? FAVORITED_CLASS : NOT_FAVORITED_CLASS
+            }
+            onClick={() => handleClickFavorite(preview.slug)}
+          >
+            <i className="ion-heart" /> {preview.favoritesCount}
           </button>
         </div>
       </div>
 
-      <CustomLink href={`/article/${article.slug}`} className="preview-link">
-        <h1>{article.title}</h1>
-        <p>{article.description}</p>
+      <CustomLink href={`/article/${preview.slug}`} className="preview-link">
+        <h1>{preview.title}</h1>
+        <p>{preview.description}</p>
         <span>Read more...</span>
         <ul className="tag-list">
-          {article.tagList.map(tag => {
+          {preview.tagList.map((tag, index) => {
             return (
-              <li className="tag-default tag-pill tag-outline" key={tag}>
-                {tag}
-              </li>
+              <Link href={`/?tag=${tag}`} key={index}>
+                <li
+                  className="tag-default tag-pill tag-outline"
+                  onClick={e => e.stopPropagation()}
+                >
+                  {tag}
+                </li>
+              </Link>
             );
           })}
         </ul>
