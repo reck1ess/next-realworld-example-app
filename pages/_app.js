564828055d59a6bfed33f83863171e42269cbe92
reck1ess <heeinso@snu.ac.kr>
2020-02-07 21:27:37 +0900

    feat: Create PageContext to paginate of ArticleList

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import React from "react";
 
+import PageContext from "../lib/context/PageContext";
 import Layout from "../components/common/Layout";
+import useSessionStorage from "../lib/hooks/useSessionStorage";
 
-export default ({ Component, pageProps }) => (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
-);
+export default ({ Component, pageProps }) => {
+  const [page, setPage] = useSessionStorage("offset", 0);
+
+  return (
+    <PageContext.Provider value={{ page, setPage: offset => setPage(offset) }}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </PageContext.Provider>
+  );
+};
