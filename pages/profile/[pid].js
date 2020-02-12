3de1875db299843ebe86240f3eb477dda363d7d7
reck1ess <heeinso@snu.ac.kr>
2020-02-12 23:50:41 +0900

    refactor: Modify profile page's following user logic

--- pages/profile/[pid].js
+++ pages/profile/[pid].js
@@ -1,5 +1,5 @@
 import React from "react";
-import useSWR, { trigger } from "swr";
+import useSWR, { mutate, trigger } from "swr";
 import { useRouter } from "next/router";
 
 import CustomImage from "../../components/common/CustomImage";
@@ -9,71 +9,66 @@ import EditProfileButton from "../../components/profile/EditProfileButton";
 import FollowUserButton from "../../components/profile/FollowUserButton";
 import ProfileTab from "../../components/profile/ProfileTab";
 import api from "../../lib/api";
-import PageContext from "../../lib/context/PageContext";
-import PageCountContext from "../../lib/context/PageCountContext";
-import useIsMounted from "../../lib/hooks/useIsMounted";
 import storage from "../../lib/utils/storage";
 import fetcher from "../../lib/utils/fetcher";
 
 import { SERVER_BASE_URL } from "../../lib/utils/constant";
 
 const Profile = ({ articles, profile: initialProfile }) => {
-  const [isFollowing, setFollowing] = React.useState(false);
   const router = useRouter();
   const {
     query: { pid }
   } = router;
 
-  React.useEffect(() => {
-    setFollowing(following);
-  }, []);
-
   /*
   Fetch remote data related with profile
   */
+
   const {
     data: fetchedProfile,
     error: profileError
-  } = useSWR(`${SERVER_BASE_URL}/profiles/${pid}`, fetcher, { initialProfile });
+  } = useSWR(
+    `${SERVER_BASE_URL}/profiles/${encodeURIComponent(pid)}`,
+    fetcher,
+    { initialProfile }
+  );
 
   if (profileError) return <RedError message="Can't load profile" />;
 
   const { profile } = fetchedProfile || initialProfile;
   const { username, bio, image, following } = profile;
-
   const { data: currentUser } = useSWR("user", storage);
   const isUser = currentUser && username === currentUser.username;
 
   const handleFollow = async () => {
-    const response = await fetch(`${SERVER_BASE_URL}/profiles/${pid}/follow`, {
+    mutate(
+      `${SERVER_BASE_URL}/profiles/${pid}`,
+      { profile: { ...profile, following: true } },
+      false
+    );
+    await fetch(`${SERVER_BASE_URL}/profiles/${pid}/follow`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Token ${encodeURIComponent(currentUser.token)}`
       }
     });
-    const {
-      profile: { following }
-    } = await response.json();
-    setFollowing(following);
-
     trigger(`${SERVER_BASE_URL}/profiles/${pid}`);
   };
 
   const handleUnfollow = async () => {
-    const response = await fetch(`${SERVER_BASE_URL}/profiles/${pid}/follow`, {
+    mutate(
+      `${SERVER_BASE_URL}/profiles/${pid}`,
+      { profile: { ...profile, following: true } },
+      true
+    );
+    await fetch(`${SERVER_BASE_URL}/profiles/${pid}/follow`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Token ${encodeURIComponent(currentUser.token)}`
       }
     });
-    const {
-      profile: { following }
-    } = await response.json();
-
-    setFollowing(following);
-
     trigger(`${SERVER_BASE_URL}/profiles/${pid}`);
   };
 
@@ -94,7 +89,7 @@ const Profile = ({ articles, profile: initialProfile }) => {
               <FollowUserButton
                 isUser={isUser}
                 username={username}
-                following={isFollowing}
+                following={following}
                 follow={handleFollow}
                 unfollow={handleUnfollow}
               />
