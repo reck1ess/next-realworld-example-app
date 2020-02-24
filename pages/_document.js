1729ae40e587513c530ea99df70a741408286ab8
reck1ess <heeinso@snu.ac.kr>
2020-02-24 23:11:37 +0900

    fix: Prevent unintentional user interaction

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,12 @@ const globalStyles = `
 img.lazyload:not([src]) {
   visibility: hidden;
 }
+* {
+  user-select: none;
+  -webkit-touch-callout: none;
+  -webkit-tap-highlight-color:rgba(255,255,255,0);
+
+}
 *:focus {
   outline: none;
 }
