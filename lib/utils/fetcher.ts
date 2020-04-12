b2bbe6142a0760d26744006fa471ec7df74cb06a
reck1ess <heeinso@snu.ac.kr>
2020-04-12 16:07:50 +0900

    feat: Add a fetcher util function

---
+++ lib/utils/fetcher.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const updateOptions = () => {
+  if (typeof window === "undefined") return;
+
+  if (!window.localStorage.user) return;
+
+  if (Object.keys(window.localStorage.user).length === 0) return;
+
+  const user = JSON.parse(window.localStorage.user);
+
+  if (!!user.token) {
+    return {
+      headers: {
+        Authorization: `Token ${user.token}`,
+      },
+    };
+  }
+};
+export default async function (url) {
+  const { data } = await axios.get(url, updateOptions());
+  return data;
+}
