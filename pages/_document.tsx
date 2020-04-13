e5cd5d1dde35c46c88cb86b2b2c149becfd8c9f2
reck1ess <heeinso@snu.ac.kr>
2020-04-13 23:15:58 +0900

    refactor: Rollback to import stylesheet in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -142,13 +142,14 @@ class MyDocument extends Document {
             }}
           />
           <link rel="manifest" href="/manifest.json" />
+          <link rel="stylesheet" href="//demo.productionready.io/main.css" />
           <link
-            href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"
             rel="stylesheet"
+            href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"
           />
           <link
-            href="//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic&display=swap"
             rel="stylesheet"
+            href="//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic&display=swap"
           />
         </Head>
         <body>
