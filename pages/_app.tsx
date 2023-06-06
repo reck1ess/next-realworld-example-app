5fa8a7f35b0c713761feed028f2e1c2b3bb2eb4e
Harry Tormey <harry.n.gale@gmail.com>
2023-06-06 10:13:01 -0700

    Add pageview logging for route changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
-import React from "react";
+import React, { useEffect } from "react";
+import { trackPageView } from "../utils/amplitude";
+import { useRouter } from 'next/router';
 
 import Layout from "components/common/Layout";
 import ContextProvider from "lib/context";
@@ -11,20 +13,35 @@ if (typeof window !== "undefined") {
   require("lazysizes");
 }
 
-const MyApp = ({ Component, pageProps }) => (
-  <>
-    <Head>
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
-      />
-    </Head>
-    <ContextProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ContextProvider>
-  </>
-);
+const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = url => {
+      console.log(`handleRouteChange with url ${url} and document.title ${document.title}`);
+      trackPageView(url)
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+
+  }, [router.events]);
+
+  return(
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+        />
+      </Head>
+      <ContextProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ContextProvider>
+    </>
+  );
+}
 
 export default MyApp;
---
+++ utils/amplitude.ts
@@ -0,0 +1,7 @@
+
+// Purpose: Amplitude analytics helper functions.
+function trackPageView(page: string): void {
+    console.log(`trackPageView page: ${page}`);
+}
+
+export { trackPageView };
